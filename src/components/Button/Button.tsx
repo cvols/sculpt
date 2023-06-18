0d6e728448736a7722564999f377b5052d87103b
cvols-valtech <chris.volonnino@valtech.com>
2023-06-18 11:33:46 -0400

    chore: update button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,44 @@
 import React from "react";
-import './Button.css';
+import "./Button.css";
 
 interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * Button contents
+   */
   label: string;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
 }
 
-export default (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+/**
+  * Primary UI component for user interaction
+*/
+const Button = ({
+  primary = false,
+  size = "medium",
+  label,
+  backgroundColor,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button type="button" style={{ backgroundColor }} {...props}>
+      {label}
+    </button>
+  );
 };
+
+export default Button;
