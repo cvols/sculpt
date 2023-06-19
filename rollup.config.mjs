cd9411d16b7e37d6702a6b92cdafd7baf9ab937c
cvols-valtech <chris.volonnino@valtech.com>
2023-06-19 10:33:43 -0400

    chore: update version to 0.1.5

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,10 +6,12 @@ import postcss from 'rollup-plugin-postcss';
 
 import packageJson from "./package.json" assert { type: "json" };
 
+// external: ["react-dom", [/\.css$/], "styled-components"],
+// plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }), postcss()],
 export default [
   {
     input: "src/index.ts",
-    external: ["react-dom"],
+    external: ["react-dom", "styled-components"],
     output: [
       {
         file: packageJson.main,
@@ -22,12 +24,12 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }), postcss()],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    globals: { "styled-components": "styled" },
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
-    external: [/\.css$/],
   }
 ];
---
+++ src/themes/darkTheme.ts
@@ -0,0 +1,17 @@
+import lightTheme from './lightTheme';
+
+const darkTheme = {
+  ...lightTheme,
+  colors: {
+    background: '#1b1c1d',
+    backgroundInverse: '#333333',
+    positive: '#9fd986',
+    negative: '#df987d',
+    primary: '#d43369',
+    secondary: '#1b8bd0',
+    tertiary: '#DDDDDD',
+    text: '#FFFFFF',
+  },
+};
+
+export default darkTheme;
