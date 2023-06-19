bf622f969a8b8e624dfa265456460022d9d362bd
cvols-valtech <chris.volonnino@valtech.com>
2023-06-19 11:55:14 -0400

    chore: update version to 0.1.7

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,33 +3,39 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import packageJson from "./package.json" assert { type: "json" };
 
-// external: ["react-dom", [/\.css$/], "styled-components"],
-// plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }), postcss()],
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.ts", // entry point for the library
     external: ["react-dom", "styled-components"],
     output: [
-      {
+      { // commonjs modules
         file: packageJson.main,
         format: "cjs",
         sourcemap: true,
       },
-      {
+      { // es6 modules
         file: packageJson.module,
         format: "esm",
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss()
+    ],
     globals: { "styled-components": "styled" },
   },
-  {
-    input: "dist/esm/index.d.ts",
+  { // typed files
+    input: "dist/esm/index.d.ts", // entry point for the library for types
+    external: [/\.css$/],
     output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
-  }
+  },
 ];
